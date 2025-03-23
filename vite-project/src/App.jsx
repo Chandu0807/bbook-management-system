ad1521583d6d5804cb20bee328460539039a31eb
= <chandrasai879@gmail.com>
2025-03-23 12:10:51 +0530

    added search section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,8 +3,34 @@ import { useState } from 'react'
 
 import BookList from './components/booklist'
 import {Books} from './utils/mockdata'
+import "./components/style.css"
 function App(){
-  return <BookList booksdata={Books}/>
+const [searchText,setSearchText]=useState("");
+const [filteredbooks,setFilteredBooks]=useState(Books);
+
+
+  function handlesearch(){
+const filterbooks=Books.filter(book=>book.title.toLowerCase().includes(searchText.toLowerCase()));
+setFilteredBooks(filterbooks);
+}
+  return (
+    <>
+    <div className='search'>
+    <h2>search books</h2>
+    <div className='search-inp'>
+    <input type="text" name="" id=""
+     className='search-input'
+    onChange={(e)=>setSearchText(e.target.value)} />
+   
+  
+    <button onClick={handlesearch}>Search</button>
+    </div>
+    </div>
+  
+      <BookList booksdata={filteredbooks}/>
+    </>
+  )
+
 }
 
 
